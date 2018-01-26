d1d7122fa3d4af8926a7d2fd134769642c24c67a
Conor Haining <conor.haining@gmail.com>
2018-01-26 10:11:13 +0000

    Test for argCount to ensure it returns only a value of 3.

--- test/inputTests.js
+++ test/inputTests.js
@@ -3,4 +3,20 @@ var input = require('../lib/input');
 
 describe("Input", function() {
 
+	describe('argCount', function() {
+		
+		it('should be no more than 3 values', function(){
+			var argv = [
+				'/usr/bin/node',
+				'/path/to/file.js',
+				'3'
+			];
+
+			var results = input.argCount(argv);
+
+			expect(results).to.equal(3);
+		});
+
+	});
+
 });
