597660dfbf4c353c0609510fa5da18da99db4740
Conor Haining <conor.haining@gmail.com>
2018-01-29 20:14:03 +0000

    First test for a small n to test for 7 values

--- test/SieveOfAtkinsTests.js
+++ test/SieveOfAtkinsTests.js
@@ -3,6 +3,18 @@ var input = require('../lib/SieveOfAtkins');
 
 describe("SieveOfAtkins", function() {
 
-	
+	describe('primeList', function() {
+
+		it('should display the first 7 primes for n = 20', function() {
+			var n = 20;
+			var validPrimes = [2, 3, 5, 7, 11, 13, 17, 19];
+
+			var primes = SieveOfAtkins.primeList(n);
+
+			expect(primes).to.equal(validPrimes);
+
+		});
+
+	});
 
 });
