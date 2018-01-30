5ff83ce061f6128e23fa9b75505c12b6779e0299
Conor Haining <conor.haining@gmail.com>
2018-01-30 12:59:43 +0000

    New test for n = 100

--- test/SieveOfAtkinsTests.js
+++ test/SieveOfAtkinsTests.js
@@ -15,6 +15,16 @@ describe("SieveOfAtkins", function() {
 
 		});
 
+		it('should display the primes for n = 100', function() {
+			var n = 100;
+			var validPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+			var primes = SieveOfAtkins.primeList(n);
+
+			expect(primes).to.deep.equal(validPrimes);
+
+		});
+
 	});
 
 });
