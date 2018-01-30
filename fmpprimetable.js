bfed928975589140376ca4ac161ea9647c187f4d
Conor Haining <conor.haining@gmail.com>
2018-01-30 14:25:21 +0000

    Logic for input validation on argv

--- fmpprimetable.js
+++ fmpprimetable.js
@@ -1,2 +1,17 @@
 var input = require('./lib/input');
 var SieveOfAtkins = require('./lib/SieveOfAtkins');
+
+// Get the arguments passed to the process
+var argv = process.argv;
+
+// Check for three arguments
+if (!input.validArgvCount(argv)) {
+	console.log('Please provide only one argument');
+	process.exit(1);
+}
+
+// Check that a valid argument has been provided
+if (!input.validInputArg(argv[2])) {
+	console.log('Please provide a positive integer');
+	process.exit(1);
+}
